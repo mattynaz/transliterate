7b4444a8431780358870471ce5227b9a71713f1f
Matthew Nazari <matthewanazari@gmail.com>
2020-12-26 15:52:14 -0800

    commit

--- key.go
+++ key.go
@@ -15,17 +15,14 @@ func LoadKey(path string) (key map[string]string, err error) {
 	defer csvfile.Close()
 
 	reader := csv.NewReader(csvfile)
-	reader.LazyQuotes = true
+	reader.FieldsPerRecord = 2
 	lines, err := reader.ReadAll()
+	
 	if err != nil {
 		return nil, fmt.Errorf(err.Error())
 	}
 
-	key = map[string]string{}
-	if len(lines) == 0 || len(lines[0]) != 2 {
-		return nil, fmt.Errorf("invalid key format, file must be CSV with two columns and one row for the column headers")
-	}
-	for _, line := range lines[1:] {
+	for _, line := range lines {
 		key[line[0]] = line[1]
 	}
 
